9a3722ca171886f0e88cf6da190249fe18de8043
AlicetheAwesome <alicetingchen@gmail.com>
2015-12-19 11:58:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,17 +7,31 @@ var heroImg = document.createElement("img");
 heroImg.src = "images/jason.gif";
 var towerImg = document.createElement("img");
 towerImg.src = "images/tower-btn.png";
+var SmallTowerImg = document.createElement("img");
+towerImg.src = "images/tower.png"
 
 var hero = {
   x: 0,
   y: 0
 };
 
+var cursor = {
+  x: 0, 
+  y: 0 
+}
+
+$("#game-cursor").mousemove(function(event)){
+  cursor.x = event.pageX;
+  cursor.y = event.pageY;
+}
+
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(heroImg, hero.x, hero.y);
   ctx.drawImage(towerImg, 575, 400, 64, 64 );
+  ctx.drawImage(SmallTowerImage, cursor.x, cursor.y);
 }
+
 
 // setTimeout(draw,1000);
 setInterval(draw, 16);
