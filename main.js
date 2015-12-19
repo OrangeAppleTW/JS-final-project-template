ce859bb8388303fce440ad569178843e299c9009
AlicetheAwesome <alicetingchen@gmail.com>
2015-12-19 11:24:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var heroImg = document.createElement("img");
 heroImg.src = "images/jason.gif";
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower.gif";
 
 var hero = {
   x: 0,
@@ -14,6 +16,7 @@ var hero = {
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(heroImg, hero.x, hero.y);
+  ctx.drawImage(towerImg,100, 100 );
 }
 
 // setTimeout(draw,1000);
